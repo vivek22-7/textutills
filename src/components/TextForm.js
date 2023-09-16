29511a4075c5f2061b3d3671a18d0ea9e2c1943b
vivek22-7 <vivek22_7@live.com>
2023-09-16 20:56:01 +0530

    Auto dissapearing default text added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,21 +4,29 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=> {
-        console.log("Upper Case was clicked!" + text);
+        
         let newText = text.toUpperCase();
         setText(newText);
     }
     const handleOnChange = (event)=> {
-        console.log("On change");
+        
         setText(event.target.value);
+    }
+    const emptytext = ()=> {
+        if(text === "Enter text here"){
+            setText("");
+        }        
     }
     const [text, setText] = useState('Enter text here');
     
+
   return (
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        {/* <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea> */}
+        <textarea className="form-control" value={text} onClick={emptytext} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+
     </div>
     <button className="btn btn-primary" onClick={handleUpClick}>Convert To UpperCase</button>
     </div>
