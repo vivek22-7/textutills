64649f6cd282eb88e529c244575585a5b405742a
vivek22-7 <vivek22_7@live.com>
2023-09-18 23:56:44 +0530

    Alert component added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,34 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
   const[Mode, setMode] = useState('light'); // Wheater dark mode is enabled or not
+  const[alert, setAlert] = useState(null);
 
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+  }
   const toggleMode = ()=>{
     if(Mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#172634';
+      showAlert("Dark Mode has been enabled", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
-
+      showAlert("Light Mode has been enabled", "success");
     }
   }
   return (
    <>    
     <Navbar title="TextUtils" aboutText = "About TextUtils" mode= {Mode} toggleMode={toggleMode}/> 
+    <Alert alert = {alert}/>
     <div className="container my-3">
       <TextForm heading= "Enter the text to analyze Below" mode={Mode}/>      
       {/* <About/> */}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,7 @@ export default function TextForm(props) {
         
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to uppercase!", "success");
     }
     const handleOnChange = (event)=> {
         
@@ -15,6 +16,7 @@ export default function TextForm(props) {
     const handleLoClick = ()=>{
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to lowercase!", "success");
     }
     const handleClClick = ()=>{
         let newText = "";
